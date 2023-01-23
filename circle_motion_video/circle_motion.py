eff3d6c086d116523b0325377f353cd3d6981bd3
Raj Singh <99217732+rajasingh310@users.noreply.github.com>
2023-01-23 18:29:38 +0100

    final version of everything

--- circle_motion_video/circle_motion.py
+++ circle_motion_video/circle_motion.py
@@ -1,27 +1,61 @@
 import numpy as np
 import cv2
+import sys
 
-# Create a black image
-img = np.zeros((512,512,3), np.uint8)
+sys.path.append("../image_generator")
+import image_generator.draw_image_using_pillow
 
-# Draw a circle
-radius = 50
-color = (0, 0, 255) # BGR
-thickness = -1
-center = (256, 256)
-cv2.circle(img, center, radius, color, thickness)
+color = image_generator.draw_image_using_pillow.color
+positions = image_generator.draw_image_using_pillow.positions
 
-# Create a video writer
-fourcc = cv2.VideoWriter_fourcc(*"mp4v") # for mp4 format
-out = cv2.VideoWriter("circle_movement.mp4", fourcc, 20.0, (512, 512))
 
-# Write the frames to the video
-for i in range(60):
-    img_copy = img.copy()
-    center = (center[0] + 5, center[1] + 5)
-    cv2.circle(img_copy, center, radius, color, thickness)
-    out.write(img_copy)
+def closest_even(n):
+    return n & ~1
 
-# Release the resources
-out.release()
-cv2.destroyAllWindows()
+
+class CircleMotionVideo:
+    def __init__(self, resolution_image=(512, 512, 3)):
+        self.resolution_image = resolution_image
+
+        # Create a black image
+        self.img = np.zeros(self.resolution_image, np.uint8)
+
+        # Circle parameters
+        self.radius = 10
+        self.color = color()  # BGR
+        self.thickness = 2
+        self.start_position = positions(self.resolution_image)
+
+    def draw_circle(self):
+        cv2.circle(self.img, self.start_position, self.radius, self.color, self.thickness)
+
+        # Create a video writer
+        self.fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # for mp4 format
+        self.out = cv2.VideoWriter("circle_movement.mp4", self.fourcc, 5.0, (self.resolution_image[0], self.resolution_image[1]))
+
+        # Write the frames to the video
+        self.n = 0
+        self.m = 0
+        for i in range(250):
+            self.img_copy = self.img.copy()
+            if i <= 10:
+                self.n = 1
+            elif i <= 20:
+                self.m = 1
+            elif i <= 30:
+                self.m = -1
+            else:
+                self.m = 1
+            self.start_position = (self.start_position[0] + self.n, self.start_position[1] + self.m)
+            cv2.circle(self.img_copy, self.start_position, self.radius, self.color, self.thickness)
+            self.out.write(self.img_copy)
+
+        # Release the resources
+        self.out.release()
+        cv2.destroyAllWindows()
+
+
+# main function to run the model
+if __name__ == "__main__":
+    motion_video = CircleMotionVideo(resolution_image=(200, 200, 3))
+    motion_video.draw_circle()
